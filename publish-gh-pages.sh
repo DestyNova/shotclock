26427519404f63f6250f40ba131fea5cfe4d19dd
Oisín Mac Fhearaí <denpashogai@gmail.com>
2018-08-11 20:33:02 +0100

    Fix publish script, hopefully

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -17,9 +17,11 @@ rm -rf .git/worktrees/public/
 echo "Checking out gh-pages branch into public..."
 git worktree add -B gh-pages public origin/gh-pages
 
+echo "Removing existing files (again?)"
+rm -rf public/*
+
 echo "Generating site"
-./clean.sh
 ./build.sh
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish-gh-pages.sh)" && git push --force origin gh-pages
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish-gh-pages.sh)" && cd .. && git push --force origin gh-pages
